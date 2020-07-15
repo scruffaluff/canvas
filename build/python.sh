beeab17433ce1cdc3b72291e1494d4fa4c3f92f5
Macklan Weinstein <wolfgangwazzlestrauss@gmail.com>
2020-07-14 18:40:47 -0700

    :arrow_up: Update Python to latest release

--- build/python.sh
+++ build/python.sh
@@ -45,13 +45,13 @@ else
     curl -Sfs https://pyenv.run | bash
 
     # Install multiple Python versions using Pyenv.
-    pyenv install 3.8.3
+    pyenv install 3.8.4
     pyenv install 3.7.8
     pyenv install 3.6.11
 
     # Set globally accessible Python versions.
     # First version is the global default.
-    pyenv global 3.8.3 3.7.8 3.6.11
+    pyenv global 3.8.4 3.7.8 3.6.11
 
     # No checks for successful Python installations since Pyenv needs to
     # source shell profiles beforehand.
